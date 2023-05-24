57faf6eecbfd6d2972a981c0848d5f767c1ba83a
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-24 11:44:55 -0500

    add add_course and list_all_students methods

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,17 @@
 class Gradebook
-  
+
+    attr_reader :instructor, :courses, :students
+    def initialize(instructor)
+        @instructor = instructor
+        @courses = []
+        @students = []
+    end
+
+    def add_course(course)
+        @courses << course
+    end
+
+    def list_all_students
+        @students
+    end
 end

f8e7874305e52eb5b9a4952058c338454ace9604
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-24 11:46:44 -0500

    test for initialization, enrollment, and if the course is full

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -5,6 +5,7 @@ require './lib/student'
 RSpec.describe Course do 
     it 'exists' do 
         course = Course.new("Calculus", 2)
+        
         expect(course.name).to eq("Calculus")
         expect(course.capacity).to eq(2)
         expect(course.students).to eq([])

74bba3bcc2fafa3a6820a178ac13f91501b3f7cd
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-24 10:12:03 -0500

    Initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
 class Student
  
+    attr_reader :name, :age, :scores,
+                :grade 
+    def initialize(name, age)
+        @name = name
+        @age = age 
+        @scores = []
+    end
+
+    def log_score(score)
+        @scores << score
+    end
+
+    def grade
+        
+    end
 end

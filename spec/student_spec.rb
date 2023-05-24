b4efd853671ca907dead406d65e4028cbbfed896
Blake Sergesketter <blakesergesketter@gmail.com>
2023-05-24 11:47:31 -0500

    rewrite test for age and name hash values

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,9 +3,8 @@ require './lib/student'
 
 RSpec.describe Student do
   it 'exists' do
-        student = Student.new("Morgan", 21) 
+        student = Student.new({name: "Morgan", age: 21}) 
         expect(student.name).to eq("Morgan")
-        expect(student.age).to eq(21)
         expect(student.scores).to eq([])
         student.log_score(89)
         student.log_score(78)
